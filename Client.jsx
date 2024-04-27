5fbe2ec1a80496804c8a63ff677aa752db088d5d
sai-cs <143716469+sai-cs@users.noreply.github.com>
2024-04-27 04:35:28 -0400

    Initial Commit

--- Client.jsx
+++ Client.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-    "https://ztmzstngpllqbcyfrint.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0bXpzdG5ncGxscWJjeWZyaW50Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjk4ODYyOSwiZXhwIjoyMDI4NTY0NjI5fQ.DWKT-nGEZdOTBIhFVpS124lbOR-UUHCrxVetVBKnoJg"
+    supaurl,
+    supakey
 )
